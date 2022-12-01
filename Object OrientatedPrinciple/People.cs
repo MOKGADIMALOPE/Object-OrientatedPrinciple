a715e06ecc96f0ab1924221d21092b227e5fb11f
MOKGADIMALOPE <104618905+MOKGADIMALOPE@users.noreply.github.com>
2022-12-01 08:27:22 +0200

    Add project files.

---
+++ Object OrientatedPrinciple/People.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace objectorientatedprinciples
+{
+    class People
+    {
+        public string name;
+        public int age;
+        public string gender;
+        public string race;
+
+        public People(string PersonName, string PersonRace, int PersonAge, string PersonGender)
+        {
+
+            name = PersonName;
+            age = PersonAge;
+            gender = PersonGender;
+            race = PersonRace;
+
+        }
+
+
+    }
+}
---
+++ Object OrientatedPrinciple/Program.cs
@@ -0,0 +1,28 @@
+﻿using objectorientatedprinciples;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        People Person1 = new People("Nomathemba", "Black", 20, "Female");
+        People Person2 = new People("Busisiwe", "Black", 10, "Female");
+        People Person3 = new People("Omphile", " Black", 27, "Male");
+
+        Console.WriteLine(Person1.name + " " + Person1.race + " " + Person1.age + " " + Person1.gender);
+        Console.WriteLine(Person2.name + " " + Person2.race + " " + Person2.age + " " + Person2.gender);
+        Console.WriteLine(Person3.name + " " + Person3.race + " " + Person3.age + " " + Person3.gender);
+
+
+
+
+
+
+    }
+
+
+
+
+}
+
+
+
